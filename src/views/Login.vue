2335e49822aa3c1f3c040f96c24645f5e975753a
savayer <sava0094@gmail.com>
2020-12-11 21:22:44 +0200

    (add) created design of the login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,50 @@
 <template>
-  <!--<input type="text" v-model="form.email">
-  {{ form.email }}-->
-  <el-row>
-    <el-col :span="8" :offset="8">
-      <el-form ref="form" :model="form" label-position="left">
-        <el-form-item label="Email">
-          <el-input type="email" v-model="form.email" />
-        </el-form-item>
-        <el-form-item label="Password">
-          <el-input type="password" v-model="form.password" />
-        </el-form-item>
-        <el-form-item label="Remember me">
-          <el-switch v-model="form.remember"></el-switch>
-        </el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-      </el-form>
-    </el-col>
-  </el-row>
+  <el-container>
+    <el-row class="block">
+
+      <el-col :span="10">
+        <div
+          ref="tilt"
+          class="tilt"
+          @mousemove="mouseMove"
+          @mouseout="mouseOut">
+          <img src="@/assets/img/login.webp" alt="">
+        </div>
+      </el-col>
+
+      <el-col :span="10" :offset="4">
+        <el-form :model="form" label-position="left">
+          <el-form-item>
+            <el-input type="email" placeholder="Email" v-model="form.email" />
+          </el-form-item>
+
+          <el-form-item>
+            <el-input type="password" placeholder="Password" v-model="form.password" />
+          </el-form-item>
+
+          <el-form-item>
+            <el-checkbox v-model="form.remember">Remember me</el-checkbox>
+          </el-form-item>
+
+          <el-button
+            round
+            type="primary"
+            @click="onSubmit">
+            Login
+          </el-button>
+
+        </el-form>
+      </el-col>
+
+    </el-row>
+  </el-container>
 </template>
 
-<script lang="ts" setup>
-  import { ref, reactive } from 'vue'
+<script lang="ts">
+  import {
+    reactive,
+    defineComponent
+  } from 'vue'
 
   interface LoginForm {
     email: string;
@@ -28,15 +52,69 @@
     remember: boolean;
   }
 
-  const form: LoginForm = reactive({
-    email: '',
-    password: '',
-    remember: false
+  export default defineComponent({
+    data: () => ({
+      tilt: null,
+      widthObject: 0,
+      heightObject: 0
+    }),
+    methods: {
+      mouseMove (e) {
+        if (!this.tilt) {
+          return
+        }
+        const xVal = e.layerX
+        const yVal = e.layerY
+        const yRotation = 20 * ((xVal - this.widthObject / 2) / this.widthObject)
+        const xRotation = -20 * ((yVal - this.heightObject / 2) / this.heightObject)
+        this.tilt.style.transform = `perspective(500px) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`
+      },
+      mouseOut () {
+        this.tilt.style.transform = 'perspective(500px) rotateX(0) rotateY(0)'
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.tilt = this.$refs.tilt
+        this.widthObject = this.tilt.clientWidth
+        this.heightObject = this.tilt.clientHeight
+      })
+    },
+    setup() {
+      const form: LoginForm = reactive({
+        email: '',
+        password: '',
+        remember: false
+      })
+
+      const onSubmit = () => {
+        alert('submit')
+      }
+
+      return {
+        form,
+        onSubmit
+      }
+    }
   })
-
-  const test = ref('hello')
-
-  const onSubmit = () => {
-    alert('submit')
-  }
 </script>
+
+<style scoped>
+  .el-container {
+    height: 100vh;
+    /*background: linear-gradient(-135deg,#c850c0,#4158d0);*/
+    background: url(../assets/img/bg-01.jpg) center center/cover no-repeat;
+  }
+
+  .block {
+    background: #fff;
+    width: 800px;
+    border-radius: 5px;
+    margin: auto;
+    padding: 4rem;
+  }
+
+  .el-button {
+    width: 100%;
+  }
+</style>
